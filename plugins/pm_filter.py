5fe23d4c6050e63cbaa74fc61ef3b3f30ed33501
PIROXTG <143400450+PIROXTG@users.noreply.github.com>
2023-10-13 20:44:59 +0530

    fix

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -5,6 +5,7 @@ import math
 import random
 lock = asyncio.Lock()
 
+from pyrogram import enums
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 import pyrogram
